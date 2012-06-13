5b9cb68a7273bdb3dbeb11bb1b281bde916f4866
Eric Sammons <elsammons@gmail.com>
2012-06-13 16:21:55 -0400

    Update setup.py to use README.md v. README.txt

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="pytest-marker-bugzilla",
     version="0.01",
     description='py.test bugzilla integration plugin, using markers',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     license='GPL',
     author='Eric Sammons',
     author_email='elsammons@gmail.com' ,
