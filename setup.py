4b4a3120e94d71d8a4247126e51304cd40425f3d
Eric Sammons <elsammons@gmail.com>
2012-06-15 13:04:52 -0400

    Update setup.py to see if we can get downloads working.

--- setup.py
+++ setup.py
@@ -9,22 +9,5 @@ setup(
     author='Eric Sammons',
     author_email='elsammons@gmail.com' ,
     url='http://github.com/eanxgeek/pytest_marker_bugzilla',
-    platforms=['linux', 'osx', 'win32'],
-    py_modules=['pytest_marker_bugzilla'],
-    entry_points = {'pytest11': ['pytest_marker_bugzilla = pytest_marker_bugzilla'],},
-    zip_safe=False,
-    install_requires = ['python-bugzilla>=0.6.2','pytest>=2.2.4'],
-    classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU General Public License (GPL)',
-    'Operating System :: POSIX',
-    'Operating System :: Microsoft :: Windows',
-    'Operating System :: MacOS :: MacOS X',
-    'Topic :: Software Development :: Testing',
-    'Topic :: Software Development :: Quality Assurance',
-    'Topic :: Utilities',
-    'Programming Language :: Python',
-    ],
-)
-
+    py_modules=['pytest-marker-bugzilla',],
+    )
