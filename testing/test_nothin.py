2e31598f81b713ab9994010219ae33d8d8669e3a
Eric Sammons <elsammons@gmail.com>
2012-06-15 13:38:02 -0400

    Updated gitignore and added sub dirs.

---
+++ testing/test_nothin.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+
+@pytest.mark.skip_selenium
+@pytest.mark.nondestructive
+class TestNothin(object):
+    @pytest.mark.bugzilla('824975')
+    def test_new_bz(self):
+        print('hello')
+        assert(os.path.exists('/etc'))
+
+    @pytest.mark.bugzilla('12345')
+    def test_closed_bz(self):
+        assert(os.path.exists('/etc'))
+
+    @pytest.mark.bugzilla('12345')
+    def test_closed_bz_with_failure(self):
+        assert(os.path.exists('/etcccc'))
+
