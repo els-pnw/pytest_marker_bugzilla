2e09097d023d5ba49ec67eac120d193372e01bf6
Eric Sammons <elsammons@gmail.com>
2012-06-15 13:36:14 -0400

    Several modifications in attempt to get setup.py right.

--- setup.py
+++ setup.py
@@ -4,9 +4,20 @@ setup(
     name="pytest-marker-bugzilla",
     version="0.01",
     description='py.test bugzilla integration plugin, using markers',
-    long_description=open('README.md').read(),
-    license='GPL',
     author='Eric Sammons',
     author_email='elsammons@gmail.com' ,
+    url='https://github.com/eanxgeek/pytest_marker_bugzilla',
     py_modules=['pytest_marker_bugzilla',],
-    )
+    license='GPL',
+    keywords='py.test pytest bugzilla',
+    entry_points={'pytest11': ['pytest_marker_bugzilla = pytest_marker_bugzilla.pytest_marker_bugzilla']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'])
